9d47c1a3f8eb4aee7b526f6229914d713f582219
littleapple27 <38861849+littleapple27@users.noreply.github.com>
2019-09-14 14:11:43 -0400

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+$(function() {
+  
+	    // Slick Carousel
+    $('#listing-carousel').slick({
+        autoplay: true, 
+        autoplaySpeed: 4000,
+		centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+		arrows: false,  
+        dots: false,
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+                arrows: false, 
+                dots: false
+            }
+        }]
+
+    });//End Slick Carousel
+
+
+
+});
+
