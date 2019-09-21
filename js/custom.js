e9a9f71d62cd61f92f4de578b2bb81b437f07dee
littleapple27 <38861849+littleapple27@users.noreply.github.com>
2019-09-21 19:15:35 -0400

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -25,27 +25,47 @@ $(function() {
 
     //slideout
     // Config Options
-    var slideout = new Slideout({
-  'panel': document.getElementById('main'),
-  'menu': document.getElementById('navbar-main'),
-  'padding': 275,
-  'tolerance': 70
-    });
+        var slideout = new Slideout({
+        'panel': document.getElementById('main'),
+        'menu': document.getElementById('navbar-main'),
+        'padding': 275,
+        'tolerance': 70
+        });
 
     // Toggle button
     document.querySelector('.toggle-button').addEventListener('click', function() {
     slideout.toggle();
     });
 
-    
+
+
+var fixed = document.querySelector('#main');
+
+slideout.on('translate', function(translated) {
+  fixed.style.transform = 'translateX(' + translated + 'px)';
+});
+
+slideout.on('beforeclose', function () {
+  fixed.style.transition = 'transform 300ms ease';
+  fixed.style.transform = 'translateX(275px)';
+});
+
+slideout.on('beforeopen', function () {
+  fixed.style.transition = 'transform 300ms ease';
+  fixed.style.transform = 'translateX(0px)';
+});
+
+slideout.on('open', function () {
+  fixed.style.transition = '';
+});
+
+slideout.on('close', function () {
+  fixed.style.transition = '';
+});
 
     // Lazy Loading
     $('#lazy-container .lazy').lazyload();
       
-    
-
-
-
 
 });
 
