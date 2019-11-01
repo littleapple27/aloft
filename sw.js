272681b354eeb7e4d2835e33b339db0a1ec0c949
littleapple27 <38861849+littleapple27@users.noreply.github.com>
2019-11-01 09:29:15 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -100,3 +100,16 @@
             );
         });
 
+//For Safari
+
+ if ('serviceWorker' in navigator) {
+        navigator.addEventListener('load', function() {
+            navigator.serviceWorker.register('sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+            });
+        });
+        }
